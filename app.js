0f93f70cdc0a127bf9c528d281b8840243400136
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-31 23:33:28 -0500

    add gitignore and ability to add multiple projects

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const inquirer = require("inquirer");
 // const fs = require('fs');
+const inquirer = require("inquirer");
 // const generatePage = require ('./src/page-template.js');
-
-// const pageHTML = generatePage(name,github);
-
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//   if (err) throw new Error(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -28,46 +20,78 @@ const promptUser = () => {
     },
   ]);
 };
-promptUser().then((answers) => console.log(answers));
+//promptUser().then(answers => console.log(answers));
 
-const promptProject = () => {
+const promptProject = (portfolioData) => {
   console.log(`
   =================
   Add a New Project 
   =================
   `);
-  return inquirer.prompt([
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer
+  .prompt([
     {
       type: "input",
       name: "name",
-      message: "What is the name of your project?"
+      message: "What is the name of your project?",
     },
     {
-      type:'checkbox',
-      name: 'languages',
-      message: 'What did you build this project with? (Check all that apply)',
-      choices:['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'] 
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
     },
     {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      type: "input",
+      name: "link",
+      message: "Enter the GitHub link to your project. (Required)",
     },
     {
-      type: 'confirm',
-      name: 'feature',
-      message: 'Would you like to feature this project?',
-      default: false
+      type: "confirm",
+      name: "feature",
+      message: "Would you like to feature this project?",
+      default: false,
     },
-    {
-      type: 'confirm',
-      name: 'confirmAddProject',
-      message: 'Would you like to enter another project?',
-      default: false
-    }
-  ]);
-};
-promptUser()
-  .then(answers => console.log(answers))
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "Would you like to enter another project?",
+        default: false,
+      },
+    ])
+    .then((projectData) => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+  };
+  
+  promptUser()
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then((portfolioData) => {
+    console.log(portfolioData);
+    
+    // const pageHTML = generatePage(name,github);
+    
+    // fs.writeFile('./index.html', generatePage(name, github), err => {
+    //   if (err) throw new Error(err);
+    
+    //   console.log('Portfolio complete! Check out index.html to see the output!');
+    // });
+  });
+  
