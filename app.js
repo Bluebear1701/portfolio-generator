99b2788d031ae4a4a14d49ea8744a75d018c5f4e
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-02 23:01:27 -0500

    copied stylesheet succesfully

--- app.js
+++ app.js
@@ -142,14 +142,19 @@ const promptProject = (portfolioData) => {
 
 promptUser()
   .then(promptProject)
-  .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, err => {
-      if (err) throw new Error(err);
-      
-      console.log(
-        "Portfolio complete! Check out index.html to see the output!"
-      );
-    });
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
